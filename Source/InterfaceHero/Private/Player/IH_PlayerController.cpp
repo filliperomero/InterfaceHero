bc69ee3850d0fa985d49645896601c35fe8dd60f
filliperomero <fillipe.romero@gmail.com>
2025-05-31 12:47:06 -0600

    Add Player Controller and Game Mode

---
+++ Source/InterfaceHero/Private/Player/IH_PlayerController.cpp
@@ -0,0 +1,3 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "InterfaceHero/Public/Player/IH_PlayerController.h"
---
+++ Source/InterfaceHero/Public/Player/IH_PlayerController.h
@@ -0,0 +1,13 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "IH_PlayerController.generated.h"
+
+UCLASS()
+class INTERFACEHERO_API AIH_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
