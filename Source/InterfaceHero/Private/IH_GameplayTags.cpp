4e6edf6b650e29321d1a1bbd2da2aa9c780c0969
filliperomero <fillipe.romero@gmail.com>
2025-05-31 22:13:54 -0600

    Add Custom Gameplay Tags

---
+++ Source/InterfaceHero/Private/IH_GameplayTags.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "IH_GameplayTags.h"
+
+namespace InterfaceHeroGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(WidgetStack_Modal, "WidgetStack.Modal", "");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(WidgetStack_GameMenu, "WidgetStack.GameMenu", "");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(WidgetStack_GameHud, "WidgetStack.GameHud", "");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(WidgetStack_Frontend, "WidgetStack.Frontend", "");
+}
---
+++ Source/InterfaceHero/Public/IH_GameplayTags.h
@@ -0,0 +1,13 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace InterfaceHeroGameplayTags
+{
+	INTERFACEHERO_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(WidgetStack_Modal);
+	INTERFACEHERO_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(WidgetStack_GameMenu);
+	INTERFACEHERO_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(WidgetStack_GameHud);
+	INTERFACEHERO_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(WidgetStack_Frontend);
+}
