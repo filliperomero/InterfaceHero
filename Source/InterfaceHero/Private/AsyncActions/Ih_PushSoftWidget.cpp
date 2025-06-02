151068be4ec31347a09714eb3616ae67be46378b
filliperomero <fillipe.romero@gmail.com>
2025-06-01 22:18:17 -0600

    Add PushSoftWidget Blueprint Async Action

---
+++ Source/InterfaceHero/Private/AsyncActions/Ih_PushSoftWidget.cpp
@@ -0,0 +1,66 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+
+#include "AsyncActions/Ih_PushSoftWidget.h"
+
+#include "Subsystems/Ih_UISubsystem.h"
+#include "Widgets/Ih_ActivatableBase.h"
+
+void UIh_PushSoftWidget::Activate()
+{
+	Super::Activate();
+
+	UIh_UISubsystem* Subsystem = UIh_UISubsystem::Get(CachedOwningWorld.Get());
+
+	Subsystem->PushSoftWidgetToStackAsync(CachedWidgetStackTag, CachedSoftWidgetClass, [this](EAsyncPushWidgetState InPushState, UIh_ActivatableBase* PushedWidget)
+	{
+		switch (InPushState)
+		{
+		case EAsyncPushWidgetState::OnCreatedBeforePush:
+			
+			PushedWidget->SetOwningPlayer(CachedOwningPlayerController.Get());
+			OnWidgetCreatedBeforePush.Broadcast(PushedWidget);
+			
+			break;
+		case EAsyncPushWidgetState::AfterPush:
+
+			AfterPush.Broadcast(PushedWidget);
+
+			if (bCachedFocusOnNewlyPushedWidget)
+			{
+				if (UWidget* WidgetToFocus = PushedWidget->GetDesiredFocusTarget())
+				{
+					WidgetToFocus->SetFocus();
+				}
+			}
+
+			SetReadyToDestroy();
+			
+			break;
+		}
+	});
+}
+
+UIh_PushSoftWidget* UIh_PushSoftWidget::PushSoftWidget(const UObject* WorldContextObject, APlayerController* OwningPlayerController, TSoftClassPtr<UIh_ActivatableBase> InSoftWidgetClass, UPARAM(meta=(Categories="WidgetStack")) FGameplayTag InWidgetStackTag, bool bFocusOnNewlyPushedWidget)
+{
+	checkf(!InSoftWidgetClass.IsNull(), TEXT("PushSoftWidgetToStack was passed a null soft widget class"))
+
+	if (GEngine)
+	{
+		if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+		{
+			UIh_PushSoftWidget* Node = NewObject<UIh_PushSoftWidget>();
+			Node->CachedOwningWorld = World;
+			Node->CachedOwningPlayerController = OwningPlayerController;
+			Node->CachedWidgetStackTag = InWidgetStackTag;
+			Node->CachedSoftWidgetClass = InSoftWidgetClass;
+			Node->bCachedFocusOnNewlyPushedWidget = bFocusOnNewlyPushedWidget;
+			
+			Node->RegisterWithGameInstance(World);
+
+			return Node;
+		}
+	}
+
+	return nullptr;
+}
---
+++ Source/InterfaceHero/Public/AsyncActions/Ih_PushSoftWidget.h
@@ -0,0 +1,42 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "Ih_PushSoftWidget.generated.h"
+
+class UIh_ActivatableBase;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPushSoftWidgetDelegate, UIh_ActivatableBase*, PushedWidget);
+
+UCLASS()
+class INTERFACEHERO_API UIh_PushSoftWidget : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Activate() override;
+	
+	UFUNCTION(BlueprintCallable, meta = (WorldContext="WorldContextObject", HidePin="WorldContextObject", BlueprintInternalUseOnly="true", DisplayName="Push Soft Widget To Widget Stack"))
+	static UIh_PushSoftWidget* PushSoftWidget(
+		const UObject* WorldContextObject,
+		APlayerController* OwningPlayerController,
+		TSoftClassPtr<UIh_ActivatableBase> InSoftWidgetClass,
+		UPARAM(meta=(Categories="WidgetStack")) FGameplayTag InWidgetStackTag,
+		bool bFocusOnNewlyPushedWidget = true);
+
+	UPROPERTY(BlueprintAssignable)
+	FOnPushSoftWidgetDelegate OnWidgetCreatedBeforePush;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnPushSoftWidgetDelegate AfterPush;
+
+private:
+	TWeakObjectPtr<UWorld> CachedOwningWorld;
+	TWeakObjectPtr<APlayerController> CachedOwningPlayerController;
+	TSoftClassPtr<UIh_ActivatableBase> CachedSoftWidgetClass;
+	FGameplayTag CachedWidgetStackTag;
+	bool bCachedFocusOnNewlyPushedWidget { false };
+};
