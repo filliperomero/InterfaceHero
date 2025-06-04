caceb706889621432376ec802345cad47be945c9
filliperomero <fillipe.romero@gmail.com>
2025-06-03 23:22:37 -0600

    Bring the Main Menu Widget when any key is pressed in the "PressAnyKey" Widget
    - Fix Style_button_Clear sound when hovered

--- Source/InterfaceHero/Private/Widgets/Ih_ActivatableBase.cpp
+++ Source/InterfaceHero/Private/Widgets/Ih_ActivatableBase.cpp
@@ -1,3 +1,14 @@
 ﻿// Copyright Fillipe Romero. All Rights Reserved.
 
 #include "Widgets/Ih_ActivatableBase.h"
+#include "Player/Ih_PlayerController.h"
+
+AIh_PlayerController* UIh_ActivatableBase::GetOwningIhPlayerController()
+{
+	if (!CachedOwningIhPlayerController.IsValid())
+	{
+		CachedOwningIhPlayerController = GetOwningPlayer<AIh_PlayerController>();
+	}
+
+	return CachedOwningIhPlayerController.IsValid() ? CachedOwningIhPlayerController.Get() : nullptr;
+}
--- Source/InterfaceHero/Public/Widgets/Ih_ActivatableBase.h
+++ Source/InterfaceHero/Public/Widgets/Ih_ActivatableBase.h
@@ -6,8 +6,17 @@
 #include "CommonActivatableWidget.h"
 #include "Ih_ActivatableBase.generated.h"
 
+class AIh_PlayerController;
+
 UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
 class INTERFACEHERO_API UIh_ActivatableBase : public UCommonActivatableWidget
 {
 	GENERATED_BODY()
+
+protected:
+	UFUNCTION(BlueprintPure)
+	AIh_PlayerController* GetOwningIhPlayerController();
+
+private:
+	TWeakObjectPtr<AIh_PlayerController> CachedOwningIhPlayerController;
 };
