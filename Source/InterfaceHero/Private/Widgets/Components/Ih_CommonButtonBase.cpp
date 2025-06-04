7a55bb1045943f6d7909abdc7d79755fc97897be
filliperomero <fillipe.romero@gmail.com>
2025-06-03 22:59:27 -0600

    Add Common Button Base Widget and create Widget from it

---
+++ Source/InterfaceHero/Private/Widgets/Components/Ih_CommonButtonBase.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Widgets/Components/Ih_CommonButtonBase.h"
+
+#include "CommonTextBlock.h"
+
+void UIh_CommonButtonBase::SetButtonText(FText InText)
+{
+	if (CommonTextBlock_ButtonText && !InText.IsEmpty())
+	{
+		CommonTextBlock_ButtonText->SetText(bUseUpperCaseForButtonText ? InText.ToUpper() : InText);
+	}
+}
+
+void UIh_CommonButtonBase::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	SetButtonText(ButtonDisplayText);
+}
---
+++ Source/InterfaceHero/Public/Widgets/Components/Ih_CommonButtonBase.h
@@ -0,0 +1,36 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "Ih_CommonButtonBase.generated.h"
+
+class UCommonTextBlock;
+
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class INTERFACEHERO_API UIh_CommonButtonBase : public UCommonButtonBase
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetButtonText(FText InText);
+
+private:
+	virtual void NativePreConstruct() override;
+	
+	/** Bound Widgets */
+	UPROPERTY(meta=(BindWidgetOptional))
+	TObjectPtr<UCommonTextBlock> CommonTextBlock_ButtonText;
+	/** Bound Widgets */
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IH Button", meta=(AllowPrivateAccess="true"))
+	FText ButtonDisplayText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IH Button", meta=(AllowPrivateAccess="true"))
+	bool bUseUpperCaseForButtonText { false };
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IH Button", meta=(AllowPrivateAccess="true"))
+	FText ButtonDescriptionText;
+};
