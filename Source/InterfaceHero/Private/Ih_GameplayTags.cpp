b99a8eafc438a31d1ba62cb265ac4f2f76531572
filliperomero <fillipe.romero@gmail.com>
2025-06-07 12:35:58 -0600

    Add Story Screen

--- Source/InterfaceHero/Private/Ih_GameplayTags.cpp
+++ Source/InterfaceHero/Private/Ih_GameplayTags.cpp
@@ -14,4 +14,5 @@ namespace InterfaceHeroGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Widget_PressAnyKeyScreen, "Widget.PressAnyKeyScreen", "");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Widget_MainMenuScreen, "Widget.MainMenuScreen", "");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Widget_ConfirmScreen, "Widget.ConfirmScreen", "");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Widget_StoryScreen, "Widget.StoryScreen", "");
 }
