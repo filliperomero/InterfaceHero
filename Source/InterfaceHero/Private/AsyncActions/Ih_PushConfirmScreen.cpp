5199c2a49cafa4469d635b54eb98470c045f269c
filliperomero <fillipe.romero@gmail.com>
2025-06-07 00:19:41 -0600

    Add Confirm Screen Widget and use it when Quit button is clicked

---
+++ Source/InterfaceHero/Private/AsyncActions/Ih_PushConfirmScreen.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "AsyncActions/Ih_PushConfirmScreen.h"
+
+#include "Subsystems/Ih_UISubsystem.h"
+
+void UIh_PushConfirmScreen::Activate()
+{
+	Super::Activate();
+
+	UIh_UISubsystem* Subsystem = UIh_UISubsystem::Get(CachedOwningWorld.Get());
+	
+	Subsystem->PushConfirmScreenToModalStackAsync(
+		CachedScreenType,
+		CachedScreenTitle,
+		CachedScreenMessage,
+		[this](EIh_ConfirmScreenButtonType ClickedButtonType)
+		{
+			OnButtonClicked.Broadcast(ClickedButtonType);
+
+			SetReadyToDestroy();
+		}
+	);
+}
+
+UIh_PushConfirmScreen* UIh_PushConfirmScreen::PushConfirmScreen(const UObject* WorldContextObject, EIh_ConfirmScreenType ScreenType, FText InScreenTitle, FText InScreenMessage)
+{
+	if (GEngine)
+	{
+		if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+		{
+			UIh_PushConfirmScreen* Node = NewObject<UIh_PushConfirmScreen>();
+			Node->CachedOwningWorld = World;
+			Node->CachedScreenType = ScreenType;
+			Node->CachedScreenTitle = InScreenTitle;
+			Node->CachedScreenMessage = InScreenMessage;
+			
+			Node->RegisterWithGameInstance(World);
+
+			return Node;
+		}
+	}
+
+	return nullptr;
+}
---
+++ Source/InterfaceHero/Public/AsyncActions/Ih_PushConfirmScreen.h
@@ -0,0 +1,36 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "Types/Ih_EnumTypes.h"
+#include "Ih_PushConfirmScreen.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnConfirmScreenButtonClickedDelegate, EIh_ConfirmScreenButtonType, ClickedButtonType);
+
+UCLASS()
+class INTERFACEHERO_API UIh_PushConfirmScreen : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Activate() override;
+	
+	UFUNCTION(BlueprintCallable, meta = (WorldContext="WorldContextObject", HidePin="WorldContextObject", BlueprintInternalUseOnly="true", DisplayName="Show Confirmation Screen"))
+	static UIh_PushConfirmScreen* PushConfirmScreen(
+		const UObject* WorldContextObject,
+		EIh_ConfirmScreenType ScreenType,
+		FText InScreenTitle,
+		FText InScreenMessage
+	);
+
+	UPROPERTY(BlueprintAssignable)
+	FOnConfirmScreenButtonClickedDelegate OnButtonClicked;
+
+private:
+	TWeakObjectPtr<UWorld> CachedOwningWorld;
+	EIh_ConfirmScreenType CachedScreenType;
+	FText CachedScreenTitle;
+	FText CachedScreenMessage;
+};
