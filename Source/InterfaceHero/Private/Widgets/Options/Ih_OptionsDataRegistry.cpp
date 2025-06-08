e0250ce5d384f84ef0274b4f17c2f44117f5bb45
filliperomero <fillipe.romero@gmail.com>
2025-06-08 01:14:51 -0600

    Add Options Data Registry to create all List Data Object Collection needed

---
+++ Source/InterfaceHero/Private/Widgets/Options/Ih_OptionsDataRegistry.cpp
@@ -0,0 +1,48 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Widgets/Options/Ih_OptionsDataRegistry.h"
+#include "Widgets/Options/DataObjects/Ih_ListDataObject_Collection.h"
+
+void UIh_OptionsDataRegistry::InitOptionsDataRegistry(ULocalPlayer* InOwningLocalPlayer)
+{
+	InitGameplayCollectionTab();
+	InitAudioCollectionTab();
+	InitVideoCollectionTab();
+	InitControlCollectionTab();
+}
+
+void UIh_OptionsDataRegistry::InitGameplayCollectionTab()
+{
+	UIh_ListDataObject_Collection* GameplayTabCollection = NewObject<UIh_ListDataObject_Collection>();
+	GameplayTabCollection->SetDataID(FName("GameplayTabCollection"));
+	GameplayTabCollection->SetDataDisplayName(FText::FromString(TEXT("Gameplay")));
+
+	RegisteredOptionsTabCollections.Add(GameplayTabCollection);
+}
+
+void UIh_OptionsDataRegistry::InitAudioCollectionTab()
+{
+	UIh_ListDataObject_Collection* AudioTabCollection = NewObject<UIh_ListDataObject_Collection>();
+	AudioTabCollection->SetDataID(FName("AudioTabCollection"));
+	AudioTabCollection->SetDataDisplayName(FText::FromString(TEXT("Audio")));
+
+	RegisteredOptionsTabCollections.Add(AudioTabCollection);
+}
+
+void UIh_OptionsDataRegistry::InitVideoCollectionTab()
+{
+	UIh_ListDataObject_Collection* VideoTabCollection = NewObject<UIh_ListDataObject_Collection>();
+	VideoTabCollection->SetDataID(FName("VideoTabCollection"));
+	VideoTabCollection->SetDataDisplayName(FText::FromString(TEXT("Video")));
+
+	RegisteredOptionsTabCollections.Add(VideoTabCollection);
+}
+
+void UIh_OptionsDataRegistry::InitControlCollectionTab()
+{
+	UIh_ListDataObject_Collection* ControlTabCollection = NewObject<UIh_ListDataObject_Collection>();
+	ControlTabCollection->SetDataID(FName("ControlTabCollection"));
+	ControlTabCollection->SetDataDisplayName(FText::FromString(TEXT("Control")));
+
+	RegisteredOptionsTabCollections.Add(ControlTabCollection);
+}
---
+++ Source/InterfaceHero/Public/Widgets/Options/Ih_OptionsDataRegistry.h
@@ -0,0 +1,31 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Ih_OptionsDataRegistry.generated.h"
+
+class UIh_ListDataObject_Collection;
+
+UCLASS()
+class INTERFACEHERO_API UIh_OptionsDataRegistry : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	// Called by options screen right after the object of type OptionsDataRegistry is created
+	void InitOptionsDataRegistry(ULocalPlayer* InOwningLocalPlayer);
+
+private:
+	void InitGameplayCollectionTab();
+	void InitAudioCollectionTab();
+	void InitVideoCollectionTab();
+	void InitControlCollectionTab();
+
+	UPROPERTY(Transient)
+	TArray<UIh_ListDataObject_Collection*> RegisteredOptionsTabCollections;
+
+public:
+	const TArray<UIh_ListDataObject_Collection*>& GetRegisteredOptionsTabCollections() const { return RegisteredOptionsTabCollections; }
+};
