605249b2bb2422c2c9b2a8e4a2a43d866ae70273
filliperomero <fillipe.romero@gmail.com>
2025-06-07 23:18:23 -0600

    Add Tab List Widget (No Functionality) and add it to the Options Screen

---
+++ Source/InterfaceHero/Private/Widgets/Components/Ih_TabListWidgetBase.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Widgets/Components/Ih_TabListWidgetBase.h"
+#include "Editor/WidgetCompilerLog.h"
+#include "Widgets/Components/Ih_CommonButtonBase.h"
+
+#if WITH_EDITOR
+void UIh_TabListWidgetBase::ValidateCompiledDefaults(IWidgetCompilerLog& CompileLog) const
+{
+	Super::ValidateCompiledDefaults(CompileLog);
+
+	if (!TabButtonEntryWidgetClass)
+	{
+		CompileLog.Error(FText::FromString(
+			TEXT("The variable TabButtonEntryWidgetClass has no valid entry specified. ") +
+			GetClass()->GetName() +
+			TEXT(" needs a valid entry widget class to function properly")
+		));
+	}
+}
+#endif
+
---
+++ Source/InterfaceHero/Public/Widgets/Components/Ih_TabListWidgetBase.h
@@ -0,0 +1,26 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonTabListWidgetBase.h"
+#include "Ih_TabListWidgetBase.generated.h"
+
+class UIh_CommonButtonBase;
+
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class INTERFACEHERO_API UIh_TabListWidgetBase : public UCommonTabListWidgetBase
+{
+	GENERATED_BODY()
+
+private:
+#if WITH_EDITOR
+	virtual void ValidateCompiledDefaults(class IWidgetCompilerLog& CompileLog) const override;
+#endif
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IH - Tab List Settings", meta=(AllowPrivateAccess="true", ClampMin="1", ClampMax="10"))
+	int32 DebugEditorPreviewTabCount { 3 };
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IH - Tab List Settings", meta=(AllowPrivateAccess="true"))
+	TSubclassOf<UIh_CommonButtonBase> TabButtonEntryWidgetClass;
+};
