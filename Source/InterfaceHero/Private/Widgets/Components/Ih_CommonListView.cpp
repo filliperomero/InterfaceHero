5dd5f3562a060a5a727a02e2c547118a2f3ea3aa
filliperomero <fillipe.romero@gmail.com>
2025-06-10 22:30:12 -0600

    Make the Common List View know which widget to generate

--- Source/InterfaceHero/Private/Widgets/Components/Ih_CommonListView.cpp
+++ Source/InterfaceHero/Private/Widgets/Components/Ih_CommonListView.cpp
@@ -1,3 +1,33 @@
 ﻿// Copyright Fillipe Romero. All Rights Reserved.
 
 #include "Widgets/Components/Ih_CommonListView.h"
+#include "Editor/WidgetCompilerLog.h"
+#include "Widgets/Options/Ih_DataAsset_DataListEntryMapping.h"
+#include "Widgets/Options/DataObjects/Ih_ListDataObject_Base.h"
+#include "Widgets/Options/ListEntries/Ih_ListEntry_Base.h"
+
+UUserWidget& UIh_CommonListView::OnGenerateEntryWidgetInternal(UObject* Item, TSubclassOf<UUserWidget> DesiredEntryClass, const TSharedRef<STableViewBase>& OwnerTable)
+{
+	if (IsDesignTime()) return Super::OnGenerateEntryWidgetInternal(Item, DesiredEntryClass, OwnerTable);
+
+	TSubclassOf<UIh_ListEntry_Base> WidgetClass = DataListEntryMapping->FindEntryWidgetClassByDataObject(CastChecked<UIh_ListDataObject_Base>(Item));
+
+	return GenerateTypedEntry<UIh_ListEntry_Base>(WidgetClass, OwnerTable);
+}
+
+#if WITH_EDITOR
+void UIh_CommonListView::ValidateCompiledDefaults(IWidgetCompilerLog& CompileLog) const
+{
+	Super::ValidateCompiledDefaults(CompileLog);
+
+	if (!DataListEntryMapping)
+	{
+		CompileLog.Error(FText::FromString(
+			TEXT("The variable DataListEntryMapping has no valid data asset assigned ") +
+			GetClass()->GetName()
+			+ TEXT(" needs a valid data asset to function properly")
+			));
+	}
+}
+#endif
+
--- Source/InterfaceHero/Public/Widgets/Components/Ih_CommonListView.h
+++ Source/InterfaceHero/Public/Widgets/Components/Ih_CommonListView.h
@@ -6,8 +6,21 @@
 #include "CommonListView.h"
 #include "Ih_CommonListView.generated.h"
 
+class UIh_DataAsset_DataListEntryMapping;
+
 UCLASS()
 class INTERFACEHERO_API UIh_CommonListView : public UCommonListView
 {
 	GENERATED_BODY()
+
+protected:
+	virtual UUserWidget& OnGenerateEntryWidgetInternal(UObject* Item, TSubclassOf<UUserWidget> DesiredEntryClass, const TSharedRef<STableViewBase>& OwnerTable) override;
+
+private:
+#if WITH_EDITOR
+	virtual void ValidateCompiledDefaults(IWidgetCompilerLog& CompileLog) const override;
+#endif
+	
+	UPROPERTY(EditAnywhere, Category = "IH - List View Settings")
+	UIh_DataAsset_DataListEntryMapping* DataListEntryMapping;
 };
