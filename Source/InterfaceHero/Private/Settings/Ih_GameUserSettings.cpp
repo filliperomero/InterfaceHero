9437aa9d7669ac06fe0956cc2d1d70ae1e8f3389
filliperomero <fillipe.romero@gmail.com>
2025-06-12 23:13:14 -0600

    Add Custom Game User Settings

---
+++ Source/InterfaceHero/Private/Settings/Ih_GameUserSettings.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Settings/Ih_GameUserSettings.h"
+
+UIh_GameUserSettings* UIh_GameUserSettings::Get()
+{
+	if (GEngine)
+	{
+		return CastChecked<UIh_GameUserSettings>(GEngine->GetGameUserSettings());
+	}
+
+	return nullptr;
+}
---
+++ Source/InterfaceHero/Public/Settings/Ih_GameUserSettings.h
@@ -0,0 +1,20 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "Ih_GameUserSettings.generated.h"
+
+UCLASS()
+class INTERFACEHERO_API UIh_GameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+
+public:
+	static UIh_GameUserSettings* Get();
+
+private:
+	UPROPERTY(Config)
+	FString CurrentGameDifficulty;
+};
