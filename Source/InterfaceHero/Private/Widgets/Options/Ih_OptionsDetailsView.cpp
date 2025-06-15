e950e21325060d6a0e32feebd69b6f44f789c399
filliperomero <fillipe.romero@gmail.com>
2025-06-15 16:31:28 -0600

    Add Options Details View Widget (with static data for now)

---
+++ Source/InterfaceHero/Private/Widgets/Options/Ih_OptionsDetailsView.cpp
@@ -0,0 +1,3 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Widgets/Options/Ih_OptionsDetailsView.h"
---
+++ Source/InterfaceHero/Public/Widgets/Options/Ih_OptionsDetailsView.h
@@ -0,0 +1,35 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Ih_OptionsDetailsView.generated.h"
+
+class UCommonRichTextBlock;
+class UCommonLazyImage;
+class UCommonTextBlock;
+
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class INTERFACEHERO_API UIh_OptionsDetailsView : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+	/** Bound Widgets */
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> CommonTextBlock_Title;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonLazyImage> CommonLazyImage_DescriptionImage;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonRichTextBlock> CommonRichText_Description;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonRichTextBlock> CommonRichText_DynamicDetails;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonRichTextBlock> CommonRichText_DisabledReason;
+	/** Bound Widgets */
+};
