8b22bc8852707cf7512d2cebd5a090cc580d8970
filliperomero <fillipe.romero@gmail.com>
2025-06-20 22:56:03 -0600

    Add List Entry Scalar for numeric settings that uses a slider (No Functionality yet)

---
+++ Source/InterfaceHero/Private/Widgets/Options/ListEntries/Ih_ListEntry_Scalar.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Widgets/Options/ListEntries/Ih_ListEntry_Scalar.h"
+
+void UIh_ListEntry_Scalar::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+}
+
+void UIh_ListEntry_Scalar::OnOwningListDataObjectSet(UIh_ListDataObject_Base* InOwningListDataObject)
+{
+	Super::OnOwningListDataObjectSet(InOwningListDataObject);
+}
+
+void UIh_ListEntry_Scalar::OnOwningListDataObjectModified(UIh_ListDataObject_Base* OwningModifiedData, EIh_OptionsListDataModifyReason ModifyReason)
+{
+}
---
+++ Source/InterfaceHero/Public/Widgets/Options/ListEntries/Ih_ListEntry_Scalar.h
@@ -0,0 +1,30 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ih_ListEntry_Base.h"
+#include "Ih_ListEntry_Scalar.generated.h"
+
+class UAnalogSlider;
+class UCommonNumericTextBlock;
+
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class INTERFACEHERO_API UIh_ListEntry_Scalar : public UIh_ListEntry_Base
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeOnInitialized() override;
+	virtual void OnOwningListDataObjectSet(UIh_ListDataObject_Base* InOwningListDataObject) override;
+	virtual void OnOwningListDataObjectModified(UIh_ListDataObject_Base* OwningModifiedData, EIh_OptionsListDataModifyReason ModifyReason) override;
+
+private:
+	/** Bound Widgets */
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess="true"))
+	TObjectPtr<UCommonNumericTextBlock> CommonNumeric_SettingValue;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget, AllowPrivateAccess="true"))
+	TObjectPtr<UAnalogSlider> AnalogSlider_SettingSlider;
+	/** Bound Widgets */
+};
