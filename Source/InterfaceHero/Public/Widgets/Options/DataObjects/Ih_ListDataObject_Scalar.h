7fb9331a2546ddc69da6d1952c6788b84ac69ce3
filliperomero <fillipe.romero@gmail.com>
2025-06-20 23:05:52 -0600

    Add Scalar List Data Object

---
+++ Source/InterfaceHero/Public/Widgets/Options/DataObjects/Ih_ListDataObject_Scalar.h
@@ -0,0 +1,29 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonNumericTextBlock.h"
+#include "Ih_ListDataObject_Value.h"
+#include "Ih_ListDataObject_Scalar.generated.h"
+
+
+UCLASS()
+class INTERFACEHERO_API UIh_ListDataObject_Scalar : public UIh_ListDataObject_Value
+{
+	GENERATED_BODY()
+
+private:
+	TRange<float> DisplayValueRange { TRange<float>(0.f, 1.f) };
+	TRange<float> OutputValueRange { TRange<float>(0.f, 1.f) };
+	float SliderStepSize { 0.1f };
+	ECommonNumericType DisplayNumericType { ECommonNumericType::Number };
+	FCommonNumberFormattingOptions NumberFormattingOptions;
+
+public:
+	LIST_DATA_ACCESSOR(TRange<float>, DisplayValueRange);
+	LIST_DATA_ACCESSOR(TRange<float>, OutputValueRange);
+	LIST_DATA_ACCESSOR(float, SliderStepSize);
+	LIST_DATA_ACCESSOR(ECommonNumericType, DisplayNumericType);
+	LIST_DATA_ACCESSOR(FCommonNumberFormattingOptions, NumberFormattingOptions);
+};
