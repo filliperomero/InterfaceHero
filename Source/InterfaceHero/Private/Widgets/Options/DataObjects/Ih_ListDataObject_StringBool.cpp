06d83732a74900431d61a76aefecb5808fcc0225
filliperomero <fillipe.romero@gmail.com>
2025-06-23 22:30:33 -0600

    Add String Bool List Data Object

---
+++ Source/InterfaceHero/Private/Widgets/Options/DataObjects/Ih_ListDataObject_StringBool.cpp
@@ -0,0 +1,49 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Widgets/Options/DataObjects/Ih_ListDataObject_StringBool.h"
+
+void UIh_ListDataObject_StringBool::OverrideTrueDisplayText(const FText& InNewTrueDisplayText)
+{
+	if (!AvailableOptionsStringArray.Contains(TrueString))
+	{
+		AddDynamicOption(TrueString, InNewTrueDisplayText);
+	}
+}
+
+void UIh_ListDataObject_StringBool::OverrideFalseDisplayText(const FText& InNewFalseDisplayText)
+{
+	if (!AvailableOptionsStringArray.Contains(FalseString))
+	{
+		AddDynamicOption(FalseString, InNewFalseDisplayText);
+	}
+}
+
+void UIh_ListDataObject_StringBool::SetTrueAsDefaultValue()
+{
+	SetDefaultValueFromString(TrueString);
+}
+
+void UIh_ListDataObject_StringBool::SetFalseAsDefaultValue()
+{
+	SetDefaultValueFromString(FalseString);
+}
+
+void UIh_ListDataObject_StringBool::OnDataObjectInitialized()
+{
+	TryInitBoolValues();
+
+	Super::OnDataObjectInitialized();
+}
+
+void UIh_ListDataObject_StringBool::TryInitBoolValues()
+{
+	if (!AvailableOptionsStringArray.Contains(TrueString))
+	{
+		AddDynamicOption(TrueString, FText::FromString(TEXT("ON")));
+	}
+
+	if (!AvailableOptionsStringArray.Contains(FalseString))
+	{
+		AddDynamicOption(FalseString, FText::FromString(TEXT("OFF")));
+	}
+}
---
+++ Source/InterfaceHero/Public/Widgets/Options/DataObjects/Ih_ListDataObject_StringBool.h
@@ -0,0 +1,28 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ih_ListDataObject_String.h"
+#include "Ih_ListDataObject_StringBool.generated.h"
+
+UCLASS()
+class INTERFACEHERO_API UIh_ListDataObject_StringBool : public UIh_ListDataObject_String
+{
+	GENERATED_BODY()
+
+public:
+	void OverrideTrueDisplayText(const FText& InNewTrueDisplayText);
+	void OverrideFalseDisplayText(const FText& InNewFalseDisplayText);
+	void SetTrueAsDefaultValue();
+	void SetFalseAsDefaultValue();
+
+protected:
+	virtual void OnDataObjectInitialized() override;
+
+private:
+	void TryInitBoolValues();
+	
+	const FString TrueString { TEXT("true") };
+	const FString FalseString { TEXT("false") };
+};
