1527495091c28d464e610344b9897a871256f61a
filliperomero <fillipe.romero@gmail.com>
2025-06-27 23:10:39 -0600

    Add Struct Types file to hold all structs
    - Add FOptionsDataEditConditionDescriptor

---
+++ Source/InterfaceHero/Public/Types/Ih_StructTypes.h
@@ -0,0 +1,36 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "Ih_StructTypes.generated.h"
+
+USTRUCT()
+struct FOptionsDataEditConditionDescriptor
+{
+	GENERATED_BODY()
+
+public:
+	void SetEditConditionFunc(const TFunction<bool()>& InEditConditionFunc) { EditConditionFunc = InEditConditionFunc; }
+	bool IsValid() const { return EditConditionFunc != nullptr; }
+	bool IsEditConditionMet() const
+	{
+		if (IsValid())
+		{
+			return EditConditionFunc();
+		}
+		
+		return true;
+	}
+	
+	FString GetDisabledRichReason() const { return DisabledRichReason; }
+	void SetDisabledRichReason(const FString& InDisabledRichReason) { DisabledRichReason = InDisabledRichReason; }
+
+	bool HasForcedStringValue() const { return DisabledForcedStringValue.IsSet(); }
+	FString GetDisabledForcedStringValue() const { return DisabledForcedStringValue.GetValue(); }
+	void SetForcedStringValue(const FString& InDisabledForcedStringValue) { DisabledForcedStringValue = InDisabledForcedStringValue; }
+	
+private:
+	TFunction<bool()> EditConditionFunc;
+	FString DisabledRichReason;
+	TOptional<FString> DisabledForcedStringValue;
+};
