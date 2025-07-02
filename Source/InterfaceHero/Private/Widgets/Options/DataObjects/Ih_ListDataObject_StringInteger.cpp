2b500ccd136ba1885e16f2a201b2ca84ab384dfa
filliperomero <fillipe.romero@gmail.com>
2025-07-01 23:38:44 -0600

    Add String Integer List Data Object to handle integer values

---
+++ Source/InterfaceHero/Private/Widgets/Options/DataObjects/Ih_ListDataObject_StringInteger.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Widgets/Options/DataObjects/Ih_ListDataObject_StringInteger.h"
+#include "Widgets/Options/Ih_OptionsDataInteractionHelper.h"
+
+void UIh_ListDataObject_StringInteger::AddIntegerOption(const int32 InIntegerValue, const FText& InDisplayText)
+{
+	AddDynamicOption(LexToString(InIntegerValue), InDisplayText);
+}
+
+void UIh_ListDataObject_StringInteger::OnEditDependencyDataModified(UIh_ListDataObject_Base* ModifiedDependencyData, EIh_OptionsListDataModifyReason ModifyReason)
+{
+	if (DataDynamicGetter)
+	{
+		CurrentStringValue = DataDynamicGetter->GetValueAsString();
+
+		if (!TrySetDisplayTextFromStringValue(CurrentStringValue))
+		{
+			CurrentDisplayText = FText::FromString(TEXT("Custom"));
+		}
+
+		NotifyListDataModified(this, EIh_OptionsListDataModifyReason::DependencyModified);
+	}
+
+	Super::OnEditDependencyDataModified(ModifiedDependencyData, ModifyReason);
+}
+
+void UIh_ListDataObject_StringInteger::OnDataObjectInitialized()
+{
+	Super::OnDataObjectInitialized();
+
+	if (!TrySetDisplayTextFromStringValue(CurrentStringValue))
+	{
+		CurrentDisplayText = FText::FromString(TEXT("Custom"));
+	}
+}
---
+++ Source/InterfaceHero/Public/Widgets/Options/DataObjects/Ih_ListDataObject_StringInteger.h
@@ -0,0 +1,21 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ih_ListDataObject_String.h"
+#include "Ih_ListDataObject_StringInteger.generated.h"
+
+UCLASS()
+class INTERFACEHERO_API UIh_ListDataObject_StringInteger : public UIh_ListDataObject_String
+{
+	GENERATED_BODY()
+
+public:
+	void AddIntegerOption(const int32 InIntegerValue, const FText& InDisplayText);
+
+	virtual void OnEditDependencyDataModified(UIh_ListDataObject_Base* ModifiedDependencyData, EIh_OptionsListDataModifyReason ModifyReason = EIh_OptionsListDataModifyReason::DirectlyModified) override;
+
+protected:
+	virtual void OnDataObjectInitialized() override;
+};
