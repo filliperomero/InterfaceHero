d1025b510cf83535c386d337c642b84efa7f3afd
filliperomero <fillipe.romero@gmail.com>
2025-07-10 23:17:16 -0600

    Add Developer Settings for Loading Screen Settings

---
+++ Source/InterfaceHero/Private/Settings/Ih_LoadingScreenSettings.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Settings/Ih_LoadingScreenSettings.h"
+#include "Blueprint/UserWidget.h"
+
+TSubclassOf<UUserWidget> UIh_LoadingScreenSettings::GetLoadingScreenWidgetClassChecked() const
+{
+	checkf(!SoftLoadingScreenWidgetClass.IsNull(), TEXT("Widget Blueprint is not set/valid in the project settings for the Loading Screen"))
+
+	TSubclassOf<UUserWidget> LoadedLoadingScreenWidget = SoftLoadingScreenWidgetClass.LoadSynchronous();
+
+	return LoadedLoadingScreenWidget;
+}
---
+++ Source/InterfaceHero/Public/Settings/Ih_LoadingScreenSettings.h
@@ -0,0 +1,25 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "Ih_LoadingScreenSettings.generated.h"
+
+UCLASS(Config=Game, defaultconfig, meta=(DisplayName="Interface Hero Loading Screen Settings"))
+class INTERFACEHERO_API UIh_LoadingScreenSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	TSubclassOf<UUserWidget> GetLoadingScreenWidgetClassChecked() const;
+	
+	UPROPERTY(Config, EditAnywhere, Category = "Loading Screen Settings")
+	TSoftClassPtr<UUserWidget> SoftLoadingScreenWidgetClass;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Loading Screen Settings")
+	float HoldLoadingScreenExtraSeconds { 3.f };
+
+	UPROPERTY(Config, EditAnywhere, Category = "Loading Screen Settings")
+	bool bShowLoadingScreenInEditor { false };
+};
