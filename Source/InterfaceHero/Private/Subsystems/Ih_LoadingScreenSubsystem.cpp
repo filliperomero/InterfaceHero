fd5d0645e6cdb5deb770ea6d01be90bf5f6340b7
filliperomero <fillipe.romero@gmail.com>
2025-07-11 22:44:09 -0600

    Add Loading Screen Subsystem (basic functionality)

---
+++ Source/InterfaceHero/Private/Subsystems/Ih_LoadingScreenSubsystem.cpp
@@ -0,0 +1,70 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Subsystems/Ih_LoadingScreenSubsystem.h"
+
+bool UIh_LoadingScreenSubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	if (!CastChecked<UGameInstance>(Outer)->IsDedicatedServerInstance())
+	{
+		TArray<UClass*> FoundClasses;
+		GetDerivedClasses(GetClass(), FoundClasses);
+
+		return FoundClasses.IsEmpty();
+	}
+
+	return false;
+}
+
+void UIh_LoadingScreenSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	FCoreUObjectDelegates::PreLoadMapWithContext.AddUObject(this, &ThisClass::OnMapPreLoaded);
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &ThisClass::OnMapPostLoaded);
+}
+
+void UIh_LoadingScreenSubsystem::Deinitialize()
+{
+	FCoreUObjectDelegates::PreLoadMapWithContext.RemoveAll(this);
+	FCoreUObjectDelegates::PostLoadMapWithWorld.RemoveAll(this);
+}
+
+UWorld* UIh_LoadingScreenSubsystem::GetTickableGameObjectWorld() const
+{
+	if (const UGameInstance* OwningGameInstance = GetGameInstance())
+	{
+		return OwningGameInstance->GetWorld();
+	}
+
+	return nullptr;
+}
+
+void UIh_LoadingScreenSubsystem::Tick(float DeltaTime)
+{
+}
+
+ETickableTickType UIh_LoadingScreenSubsystem::GetTickableTickType() const
+{
+	if (IsTemplate())
+	{
+		return ETickableTickType::Never;
+	}
+
+	return ETickableTickType::Conditional;
+}
+
+bool UIh_LoadingScreenSubsystem::IsTickable() const
+{
+	return GetGameInstance() && GetGameInstance()->GetGameViewportClient();
+}
+
+TStatId UIh_LoadingScreenSubsystem::GetStatId() const
+{
+	RETURN_QUICK_DECLARE_CYCLE_STAT(UIh_LoadingScreenSubsystem, STATGROUP_Tickables);
+}
+
+void UIh_LoadingScreenSubsystem::OnMapPreLoaded(const FWorldContext& WorldContext, const FString& MapName)
+{
+}
+
+void UIh_LoadingScreenSubsystem::OnMapPostLoaded(UWorld* LoadedWorld)
+{
+}
---
+++ Source/InterfaceHero/Public/Subsystems/Ih_LoadingScreenSubsystem.h
@@ -0,0 +1,30 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Ih_LoadingScreenSubsystem.generated.h"
+
+UCLASS()
+class INTERFACEHERO_API UIh_LoadingScreenSubsystem : public UGameInstanceSubsystem, public FTickableGameObject
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	/** FTickableGameObject Interface */
+	virtual UWorld* GetTickableGameObjectWorld() const override;
+	virtual void Tick(float DeltaTime) override;
+	virtual ETickableTickType GetTickableTickType() const override;
+	virtual bool IsTickable() const override;
+	virtual TStatId GetStatId() const override;
+	/** FTickableGameObject Interface */
+
+private:
+	void OnMapPreLoaded(const FWorldContext& WorldContext, const FString& MapName);
+	void OnMapPostLoaded(UWorld* LoadedWorld);
+};
